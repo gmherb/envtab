35c691028fef6af60d6dda93a2832f409f4a6d18
Your Name <you@example.com>
2024-02-13 07:15:31 -0500

    rework tempates_test

--- cmd/envtab/templates_test.go
+++ cmd/envtab/templates_test.go
@@ -8,26 +8,34 @@ import (
 
 func TestMakeLoadoutFromTemplate(t *testing.T) {
 
-	if _, err := os.Stat(InitEnvtab("") + "/templates"); os.IsNotExist(err) {
-		err := os.Mkdir(InitEnvtab("")+"/templates", 0755)
+	name := "TestMakeLoadoutFromTemplate"
+	templateDir := InitEnvtab("") + "/templates"
+	filepath := templateDir + "/" + name + ".yml"
+
+	if _, err := os.Stat(templateDir); os.IsNotExist(err) {
+		err := os.Mkdir(templateDir, 0755)
 		if err != nil {
-			t.Errorf("Error creating test template: %v", err)
+			t.Errorf("Error creating %s template: %v", filepath, err)
 		}
 	}
 
-	f, err := os.Create(InitEnvtab("") + "/templates/test.yml")
+	f, err := os.Create(filepath)
 	if err != nil {
-		t.Errorf("Error creating test template: %v", err)
+		t.Errorf("Error creating %s template: %v", filepath, err)
 	}
 
 	_, err = f.WriteString("entries:\n- AWS_ACCESS_KEY_ID\n")
 	if err != nil {
-		t.Errorf("Error writing test template: %v", err)
+		t.Errorf("Error writing %s template: %v", filepath, err)
 	}
 
-	loadout := MakeLoadoutFromTemplate("test", false)
+	loadout := MakeLoadoutFromTemplate(name, false)
 	fmt.Printf("DEBUG: Loadout: %v\n", loadout)
 
+	if loadout.Entries["AWS_ACCESS_KEY_ID"] != "" {
+		t.Errorf("Error creating %s template: %v", filepath, "AWS_ACCESS_KEY_ID should be empty")
+	}
+
 	// cleanup
-	err = os.Remove(InitEnvtab("") + "/templates/test.yml")
+	err = os.Remove(filepath)
 }
